fbceea1c5b06931e2d35dc36eaeb1514ee7a4232
Gabriel <gaby.tzu@windowslive.com>
2020-08-10 12:25:13 +0300

    Use BeautifulSoup instead of Selenium

--- main.py
+++ main.py
@@ -1,55 +1,36 @@
-import ezsheets, os, datetime, threading, time
-from selenium import webdriver
+import ezsheets
+import datetime
+import threading
+import time
+import requests
+import os
+from bs4 import BeautifulSoup
 
-os.chdir("C:\\Users\\user\\Desktop\\programming\\Python")
+os.chdir("C:\\Users\\Gabriel\\Desktop\\programming\\Python")
 pricesRetrieved = False 
 symbolDict = {"BRD": 0, "TLV": 0, "SNG": 0, "SNN": 0, "FP": 0}
+link = "http://www.bvb.ro/FinancialInstruments/Details/FinancialInstrumentsDetails.aspx?s="
 
 # retrieve the prices from bvb's website
 def stock():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-plugins-discovery")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--incognito")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
-    browser = webdriver.Chrome(executable_path=r"C:\Users\user\Desktop\programming\Python\chromedriver.exe", options=options)
-    browser.delete_all_cookies()
-    browser.implicitly_wait(10)         # wait 10 seconds for each element to be available
-    browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-    "source": """
-        Object.defineProperty(navigator, 'webdriver', {
-        get: () => undefined
-        })
-    """
-    })
-    browser.execute_cdp_cmd("Network.enable", {})
-    browser.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser1"}})
-    link = "http://www.bvb.ro/FinancialInstruments/Details/FinancialInstrumentsDetails.aspx?s="
     print("Accessing BVB's website...")
-
     for i in symbolDict:
-        browser.get(link + i)                                       #build the link
-        price = browser.find_element_by_class_name("value").text
-        print("The price for " + i + " is: " + price)               #get the current price
+        url = link + i
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+        data = requests.get(url=url, headers={'user-agent': user_agent})
+        soup = BeautifulSoup(data.text, "html.parser")
+        x = soup.find_all("strong", class_="value")  
+        x = str(x).split(">")
+        x = x[1].split("<")
+        price = x[0]
+        print("The price for " + i + " is: " + str(price))              
         price = price.replace(".", ",")                             #replace the dot (.) with comma (,) for Google Sheets' format to recognize the price correctly
         symbolDict[i] = price
-        
-    print(symbolDict)
-    browser.quit()
-
-    global pricesRetrieved    
-    pricesRetrieved = True
-    return pricesRetrieved
 
 # save the results to Google Sheets
 def googleSheets():
-
     sheets = ["BRD", "BANCA TRANSILVANIA", "ROMGAZ", "NUCLEARELECTRICA", "FONDUL PROPRIETATEA"]
-    # assign the Google Sheets workbook id
-    ss = ezsheets.Spreadsheet("")
+    ss = ezsheets.Spreadsheet("1zQJxdZZNFXsmOFWYEe3NdMaS2scpMr_BBE2SOJbecdI")
     print("Accessing the Google Sheet '" + str(ss.title) + "'...")
 
     j = 0
@@ -61,33 +42,28 @@ def googleSheets():
         print(i + ": " + symbolDict[i])
         j += 1
     
-    now = datetime.datetime.now()       #retrieve the current time
-    sheet = ss["Dashboard"]             #change the active sheet to the 'Dashboard' sheet
+    now = datetime.datetime.now()      
+    sheet = ss["Dashboard"]         
 
     # use zfill to add leading "0" to one digit values
     sheet["G5"] = "LAST KNOWN MARKET VALUE (" + str(now.day).zfill(2) + "/" + str(now.month).zfill(2) + "," + str(now.hour).zfill(2) + ":" + str(now.minute).zfill(2) + ")"
-    sheet = ss["historicData"]          #change the active sheet to the 'historicData' sheet
+    sheet = ss["historicData"]         
     print("Accessing sheet '" + str(sheet.title) + "'...")
-
+    # find the last row
     i = 1
     while sheet.get(1,i) != '':
         i+=1
-    lastRow = i                         #find the last row
-    # add the date on the last row
+    lastRow = i                        
     print("Adding date and time in the row: " + str(lastRow))
     sheet[1, lastRow] = str(now.day).zfill(2) + "/" + str(now.month).zfill(2) + "," + str(now.hour).zfill(2) + ":" + str(now.minute).zfill(2)
-    
+    # add the prices for each symbol on the last row
     j = 2
-    for i in symbolDict:                #add the prices for each symbol on the last row
+    for i in symbolDict:                
         print("Adding price " + symbolDict[i] + " for symbol " + i + " on column " + str(j) + ", row " + str(lastRow) + "...")
         sheet[j, lastRow] = symbolDict[i]
         j += 1 
 
-    ss.refresh()
-
 # call the stock() function in advance
 threadObj = threading.Thread(target=stock)
 threadObj.start()
-
-# call the google sheets() function
 googleSheets()
